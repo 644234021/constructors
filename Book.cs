bfb86abc2d5c821fd919b38d3ad36a257b1b64d2
ACER <ACER@LAPTOP-543ACTVA>
2023-01-08 22:05:12 +0700

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW_constructors
+{
+    internal class Book
+    {
+        String BookTitle;
+        String Author;
+        int PublishYear;
+        String ISBN;
+        int PageCount;
+
+        public Book()
+        {
+            BookTitle = "Unknown";
+            Author = "Unknown";
+            ISBN = "Unknown";
+            PublishYear= 0;
+            PageCount = 0;
+        }
+
+        public void booktitle1(String ti,String au)
+        {
+            BookTitle = ti;
+            Author = au;
+        }
+
+        public void booktitle2(String ti,String au ,int year)
+        {
+            BookTitle = ti;
+            Author = au;
+            PublishYear = year;
+        }
+        public void display()
+        {
+            Console.WriteLine("Book title : {0}. ", BookTitle);
+            Console.WriteLine("Author : {0}. ", Author);
+            Console.WriteLine("ISBN : {0}.", ISBN);
+            Console.WriteLine("Publish Year : {0}", PublishYear);
+            Console.WriteLine("Page Count : {0}", PageCount);
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book();
+            book2.booktitle1("Fouyou","Jon_Newkrater");
+            Book book3 = new Book();
+            book3.booktitle2("Goodnight", "JAME", 2560);
+            book1.display();
+            book2.display();
+            book3.display();
+            Console.ReadKey();
+        }
+    }
+}
